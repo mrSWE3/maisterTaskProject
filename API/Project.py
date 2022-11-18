65ecb08b702d8f4b2b3379ddf64c5b855b9a1630
KristofferGus <gusguskrp@student.gu.se>
2022-11-18 18:54:52 +0100

    Finished design somewhat

--- API/Project.py
+++ API/Project.py
@@ -3,7 +3,7 @@ from Section import Section
 
 class Project:
 
-    def __init__(self, name: str = "", sections: [Section] = [], dictionary: dict = {}):
+    def __init__(self, name: str = "", sections: list = [], dictionary: dict = {}):
         self.name = name
         self.sections = sections
         self.dictionary = dictionary
